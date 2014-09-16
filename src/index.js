a84bee0179d2cbdc967f44a2104940f654ddada9
Caden Lovelace <caden@herostrat.us>
2014-09-16 10:44:57 +0100

    Convert script to safely use subdir for git wrangling and pull into main git repo.

--- src/index.js
+++ src/index.js
@@ -1,82 +1,55 @@
 // Publish task
-var exec = require('child_process').exec
-  , path = require('path')
-  , rimraf = require('rimraf')
-  , fs = require('fs')
+var exec = require('child_process').exec,
+  path = require('path'),
+  rimraf = require('rimraf'),
+  fs = require('fs')
 ;
 
 function buildBranch(options, callback) {
 
-  var curBranch = 'master'
-    , execOptions = {}
-    , command = ''
+  var execOptions = {},
+    command = ''
   ;
 
   // Checking options
   options.folder = options.folder || 'www';
   options.branch = options.branch || 'gh-pages';
-  options.ignore = options.ignore || [];
-  options.ignore.push('.git', 'node_modules', options.folder);
   options.cname = options.cname || 'CNAME';
   options.commit = options.commit || 'Build '+(new Date());
   options.cwd = options.cwd || process.cwd();
   execOptions.cwd = options.cwd;
 
-  // Remember the current branch
-  command = 'git rev-parse --abbrev-ref HEAD'
+  // Switch to ghpages branch
+  fs.exists(path.join(options.cwd, options.folder, '.git'), function(exists) {
 
-  exec(command, execOptions, function(err, stdout, stderr) {
-    if(err) {
-      callback(err); return;
+    if(!exists) {
+      command = 'git init ' + options.folder;
+    }else{
+      command = ':'; // noop
     }
 
-    curBranch = stdout.trim();
-
-    // Switch to ghpages branch
-    command = 'git branch -D ' + options.branch + ';'
-      +' git checkout --orphan ' + options.branch + ';'
-      +' git rm -r --cached .';
-
     exec(command, execOptions, function(err) {
-      var ignore;
       if(err) {
         callback(err); return;
       }
 
-      // delete all files except the untracked ones
-      ignore = options.ignore.slice(0);
-      fs.readdirSync(options.cwd).forEach(function(file) {
-        if(-1 === ignore.indexOf(file)) {
-          rimraf.sync(path.join(options.cwd, file));
-        }
-      });
-      fs.readdirSync(path.join(options.cwd, options.folder))
-        .forEach(function(file) {
-          fs.renameSync(path.join(options.cwd, options.folder, file),
-            path.join(options.cwd, file));
-        });
-      fs.rmdirSync(path.join(options.cwd, options.folder));
-
       // Add the domain cname field
       if(options.domain) {
-        fs.writeFileSync(path.join(options.cwd, options.cname), options.domain);
+        fs.writeFileSync(path.join(options.cwd, options.folder, options.cname), options.domain);
       }
 
-      // Add a new ignore file
-      ignore.push('.gitignore');
-      fs.writeFileSync(path.join(options.cwd, '.gitignore'), ignore.join('\n'));
-
       // Commit files
-      command = 'git add .;'
-              + ' git commit -m "' + options.commit.replace('"', '\\"') + '"';
+      command = '(cd ' + options.folder + ' && ' +
+              'git add -A . && ' +
+              'git commit -m "' + options.commit.replace('"', '\\"') + '" )';
+
       exec(command, execOptions, function(err) {
         if(err) {
-          callback(err); return;
+          callback(); return;
         }
 
-        // Pushing commit
-        command = ' git checkout ' + curBranch + ' ;'
-                + ' git checkout .';
+        // Fetch branch into our repo.
+        command = 'git fetch ' + options.folder + ' master:' + options.branch;
 
         exec(command, execOptions, function(err) {
           if(err) {
@@ -92,4 +65,3 @@ function buildBranch(options, callback) {
 }
 
 module.exports = buildBranch;
-
